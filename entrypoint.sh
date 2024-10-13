cba952218b3993fd6689c5c0ab2da89a8b72dd47
Maximilian Hiemer <maximilianhiemer@MacBook-Air-von-Maximilian.local>
2024-10-13 12:34:40 +0200

    Bugfixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,21 @@
-#!/bin/bash -e
+#!/bin/bash
+
+# Enable debug mode and print each command before executing
+set -x
 
 printf "########################################\n"
 printf "# Container starting up!\n"
 printf "########################################\n"
 
-# Check for WebDav user/pass for Digest Authentication
+# Check for WebDav user/pass
 printf "# STATE: Checking for WebDav user/pass\n"
 if [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ]; then
     if [ ! -f /etc/apache2/webdav_credentials ]; then
         printf "# STATE: WebDav credentials file does not exist, creating it...\n"
-        htdigest -c /etc/apache2/webdav_credentials "Restricted" $WEBDAV_USER $WEBDAV_PASS
+        htdigest -c /etc/apache2/webdav_credentials "Restricted" "$WEBDAV_USER" "$WEBDAV_PASS" 2>&1
     else
         printf "# STATE: WebDav credentials file exists, adding/updating user...\n"
-        htdigest /etc/apache2/webdav_credentials "Restricted" $WEBDAV_USER $WEBDAV_PASS
+        htdigest /etc/apache2/webdav_credentials "Restricted" "$WEBDAV_USER" "$WEBDAV_PASS" 2>&1
     fi
 else
     printf "# WARN: No WebDav user/pass were set, the \"restricted\" directory has no authentication!\n"
@@ -30,7 +33,7 @@ fi
 
 # Run Apache configuration test to check for any issues
 printf "# STATE: Running Apache configtest\n"
-apachectl configtest
+apachectl configtest 2>&1
 
 printf "# STATE: Apache is starting up now, the logs you see below are error_log and access_log from Apache\n"
 
