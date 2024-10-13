189d8172ed7ffcf1e075ed9a35fd4bd572282b32
Maximilian Hiemer <maximilianhiemer@MacBook-Air-von-Maximilian.local>
2024-10-13 12:27:50 +0200

    Added debug to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ printf "########################################\n"
 printf "# Container starting up!\n"
 printf "########################################\n"
 
-# Check for WebDAV user/pass for Digest Authentication
+# Check for WebDav user/pass
 printf "# STATE: Checking for WebDav user/pass\n"
-if [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ]
-then
+if [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_PASS" ]; then
     printf "# STATE: WebDav user/pass written to /etc/apache2/webdav_credentials\n"
-    htdigest -c /etc/apache2/webdav_credentials "Restricted" $WEBDAV_USER $WEBDAV_PASS > /dev/null 2>&1
+    htdigest -c /etc/apache2/webdav_credentials "Restricted" $WEBDAV_USER $WEBDAV_PASS
 else
     printf "# WARN: No WebDav user/pass were set, the \"restricted\" directory has no authentication!\n"
     sed -i "s/.*AuthType Digest.*//g" /etc/apache2/sites-enabled/webdav.conf
@@ -19,13 +18,16 @@ else
 fi
 
 # Check for client_max_body_size equivalent for Apache (LimitRequestBody)
-if [ -n "$APACHE_CLIENT_MAX_BODY_SIZE" ]
-then
+if [ -n "$APACHE_CLIENT_MAX_BODY_SIZE" ]; then
     printf "# STATE: Setting client_max_body_size (LimitRequestBody) to $APACHE_CLIENT_MAX_BODY_SIZE\n"
     sed -i "s/LimitRequestBody 262144000/LimitRequestBody $APACHE_CLIENT_MAX_BODY_SIZE/g" /etc/apache2/sites-enabled/webdav.conf
 fi
 
+# Run Apache configuration test to check for any issues
+printf "# STATE: Running Apache configtest\n"
+apachectl configtest
+
 printf "# STATE: Apache is starting up now, the logs you see below are error_log and access_log from Apache\n"
 
 # Start Apache in the foreground
-exec "$@"
+exec apachectl -D FOREGROUND
